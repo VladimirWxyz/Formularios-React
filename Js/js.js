d9e0b58bd4f0e737f0b195f9f0c88834e23ae9c0
VladimirWxyz <vladimirwilliams.xyz@gmail.com>
2022-08-30 00:06:34 -0400

    new files

---
+++ Js/js.js
@@ -0,0 +1,73 @@
+var nombre = "Test";
+
+alert (nombre);
+
+var name;
+
+name = prompt ("Enter your name", "Number")
+
+alert (name);
+
+console.log(name);
+
+document.write(name);
+
+Test = prompt ("Enter name");
+Age = prompt ("Age");
+Ag = 20 - 3;
+
+document.write(Test + " " + Age + " " + Ag);
+
+//Tipos de datos
+
+    // Numerico
+
+    var edad = 19;
+
+    // String
+
+    var nombre = "Vladimir ";
+    var apellido = "Williams ";
+    var texto = "Text of \"test\"";
+
+    document.write(nombre + apellido + texto);
+
+    // Array
+
+    var friends = ["Albert", " Vladimir ", "Williams"]
+
+    friends[friends.length] = "Abduzcan";
+
+    document.write(friends[1]);
+
+    document.write("Tienes " + friends.length + " amigos");
+
+    // Object
+
+    var object = {
+        color: "Black",
+        size: 12
+    }
+
+    // Boolean
+
+    var bolean = false;
+
+    //Undefined
+
+    var testeo;
+    document.write(testeo);
+
+    // Null
+
+    var numtest1 = 10;
+    var numtest1 = null;
+
+    // Nan
+
+    var text1 = "hello";
+    document.write(text1 * 2)
+
+    // NaN es un valor invalido en js
+
+    
--- React/HelloWorld.jsx
+++ React/HelloWorld.jsx
@@ -1,3 +1,23 @@
 const root =
 ReactDom.createRoot(document.getElementById('root'));
 root.render(<h1>Hello, World!</h1>)
+
+
+const name = 'Albert Williams';
+const elemento = <h1>Hello, {name}</h1>;
+
+
+function formatname(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Vladimir',
+    lastName: 'Williams'
+};
+
+const element = (
+    <h1>
+    Hello, {formatname(user)}
+    </h1>
+);
